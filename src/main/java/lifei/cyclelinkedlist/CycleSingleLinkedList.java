4e75507d98551d6fc5892c8b83d37bb351267464
Fei Li <fei.li@healthbi.com>
2020-01-16 16:19:55 -0700

    补充了针对约瑟夫问题的环形链表实现

--- src/main/java/lifei/cyclelinkedlist/CycleSingleLinkedList.java
+++ src/main/java/lifei/cyclelinkedlist/CycleSingleLinkedList.java
@@ -8,38 +8,118 @@ package lifei.cyclelinkedlist;
  */
 public class CycleSingleLinkedList {
 
-    private ListNode head = new ListNode(0);
+    // 创建一个first节点，用来指向第一个节点
+    private ListNode first = new ListNode(-1);
 
     public ListNode getHead() {
-        return head;
+        return first;
     }
 
     /**
-     * 
+     * 添加节点
      * 
      * @param num
      */
     public void add(int num) {
         if (num < 1) {
+            System.out.println("num需要至少一个节点");
             return;
         }
 
-        ListNode first = null;
         ListNode cur = null;
-        ListNode temp = null;
 
         for (int i = 1; i <= num; i++) {
+            ListNode temp = new ListNode(i);
             if (i == 1) {
-                first = new ListNode(1);
+                first = temp;
+                first.next = first;
                 cur = first;
-                cur.next = first;
             } else {
-                temp = new ListNode(i);
                 cur.next = temp;
                 temp.next = first;
                 cur = temp;
             }
         }
+    }
+
+    /**
+     * 打印环形链表
+     * 
+     */
+    public void display() {
+        if (first == null) {
+            System.out.println("空链表没有节点");
+            return;
+        }
+
+        // 定义一个辅助指针 指向头节点
+        ListNode curr = first;
+        while (true) {
+            System.out.printf("结点的编号是 : %d\n", curr.no);
+            if (curr.next == first) {
+                break;
+            }
+            curr = curr.next;
+        }
+    }
+
+    /**
+     * 根据用户的输入，计算出节点出环的顺序
+     * 
+     * @param start 从第几个节点开始数数
+     * @param count 数了几次
+     * @param nums 初始的节点个数
+     */
+    public void count(int start, int count, int num) {
+
+        // 数据校验
+        // 思路一： 是否是空指针
+        // 思路二：开始的节点序号不能小于1，只能是1 <= start <= num
+        if (first == null || start < 1 || start > num) {
+            System.out.println("参数输入有误，请重新输入");
+            return;
+        }
+
+        ListNode helper = first;
+
+        // 需要将helper移到头指针的前一个位置
+        while (true) {
+            if (helper.next == first) {
+                break;
+            }
+            helper = helper.next;
+        }
+
+        // 需要将头指针从start位置开始
+        for (int i = 0; i < start - 1; i++) {
+            first = first.next;
+            helper = helper.next;
+        }
+
+        // 开始出环
+        while (true) {
+            if (helper == first) { // 只有一个节点
+                break;
+            }
+
+            for (int i = 0; i < count - 1; i++) {
+                first = first.next;
+                helper = helper.next;
+            }
+
+            System.out.printf("出环的节点是: %d\n", first.no);
+            first = first.next;
+            helper.next = first;
+        }
+        System.out.printf("最后一个留在圈里的节点序号是:%d\n", first.no);
+    }
+
+    public static void main(String[] args) {
+        CycleSingleLinkedList cll = new CycleSingleLinkedList();
+        cll.add(5);
+        cll.display();
+
+        cll.count(1, 2, 5);
     }
 }
 
