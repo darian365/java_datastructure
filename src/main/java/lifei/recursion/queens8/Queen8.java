46ace3224faaec2b1a675c030a31fb1e00362314
Fei Li <fei.li@healthbi.com>
2020-02-06 07:39:01 -0700

    添加八皇后的实现逻辑和注释

--- src/main/java/lifei/recursion/queens8/Queen8.java
+++ src/main/java/lifei/recursion/queens8/Queen8.java
@@ -17,4 +17,58 @@ package lifei.recursion.queens8;
  */
 public class Queen8 {
 
+    // 定义一个max表示共有多少个皇后
+    int max = 8;
+
+    // 定义数组array, 保存皇后的位置的结果，比如array = {0, 4, 7, 5, 2, 6, 1, 3}
+    int[] array = new int[max];
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 放置第n个皇后
+     * 
+     * @param int n
+     * 
+     */
+    private void place(int n) {
+        if (n >= max) {
+            System.out.printf("%d个皇后已摆放完毕", n);
+            return;
+        }
+        // 依次每列放入皇后，并判断是否冲突
+        for (int i = 0; i < max; i++) {
+            array[n] = i;
+            // 判断第n个皇后到达第i列时，是否有冲突
+            if (validate(n)) {
+                // 不冲突则放下一行的皇后
+                place(n + 1);
+            }
+            // 如果冲突，就继续执行array[n] = i(继续循环内后移),即将第n个皇后，放置在本行的后移的位置
+        }
+    }
+
+    /**
+     * 查看当我们放置第n个皇后，检测该皇后是否和已拜访的皇后冲突，即不在同一行或同一列或同一斜线
+     * 
+     * @param n 第n个皇后
+     * @return
+     */
+    private boolean validate(int n) {
+        for(int i = 0; i < n; i ++) {
+            // 如果是同一列
+            if (array[i] == array[n]) {
+                return false;
+            }
+            // 如果是同斜线
+            else if (Math.abs(n - i) == Math.abs(array[n] - array[i])) {
+                return false;
+            }
+            // 如果是同一行，不需要判断，因为n是递增的（本次n和下次 n相比较）
+        }
+        return true;
+    }
+
 }
