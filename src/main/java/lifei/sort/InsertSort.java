e050f37469a90d190f1edb3e317976e34efd815c
Fei Li <fei.li@healthbi.com>
2020-02-09 23:39:24 -0700

    加入了插入排序的注释

--- src/main/java/lifei/sort/InsertSort.java
+++ src/main/java/lifei/sort/InsertSort.java
@@ -2,6 +2,15 @@ package lifei.sort;
 
 import java.util.Arrays;
 
+/**
+ * “基本思想：
+ * “把n个待排序的元素看成一个有序表和一个无序表，开始时有序表中只包含一个元素，无序表中包含n-1个元素，
+ * “排序过程中每次从无序表中取出第一个元素，把它的排序码依次和有序表元素的排序码进行比较，将它插入到
+ * “有序表中的适当位置，使之成为新的有序表
+ * 
+ * @author Fei.Li
+ *
+ */
 public class InsertSort {
 
     public static void sort(int[] arr) {
@@ -10,15 +19,23 @@ public class InsertSort {
         }
 
         for (int i = 1; i < arr.length; i++) {
-
+            // “定义待插入的数，默认是i位置
             int insertValue = arr[i];
+            // “定义待插入的索引位置
             int insertIndex = i - 1;
 
+            // “给insertVal找到插入的位置
+            // “说明
+            // “1. insertIndex >= 0 保证在给insertValue找插入位置时不越界
+            // “2. insertValue < arr[insertIndex] 待插入的数，还没有找到插入位置
+            // “3. 就需要将arr[insertIndex]后移
+            // “4. 通过演绎推导，循环内会将比待插入的值大的都逐个后移，最后insertIndex == -1 跳出循环
             while (insertIndex >= 0 && insertValue < arr[insertIndex]) {
                 arr[insertIndex + 1] = arr[insertIndex];
                 insertIndex--;
             }
 
+            // “当while循环结束时，说明插入位置找到了，即insertIndex + 1
             arr[insertIndex + 1] = insertValue;
         }
     }
