166aba62bdfe6c7d812c041ecfc614042bd7c167
Fei Li <fei.li@healthbi.com>
2020-02-10 22:48:55 -0700

    希尔排序和快速排序

---
+++ src/main/java/lifei/sort/QuickSort.java
@@ -0,0 +1,69 @@
+package lifei.sort;
+
+public class QuickSort {
+
+    static void sort(int[] arr) {
+
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    static void quickSort(int[] arr, int start, int end) {
+
+        // ‘判断start和end的位置，至少需要左右相邻，不能交叉或重叠
+        if (start >= end) {
+            return;
+        }
+
+        int left = start;
+        int right = end;
+
+        // ‘拿到中间的值作为枢纽
+        int mid = (start + end) / 2;
+        int pivot = arr[mid];
+
+        // ‘循环开始，如果left大于right，则循环结束
+        while (left <= right) {
+            // ‘左侧找大于pivot的值，没找到的话则继续从左向右滑行
+            while (left <= right && arr[left] < pivot) {
+                left++;
+            }
+            // ‘右侧找小于pivot的值，没找到的话则继续从右向左滑行
+            while (left <= right && arr[right] > pivot) {
+                right--;
+            }
+
+            // ‘离开上面两个子循环的地方，说明left和right的位置分别是左侧大于pivot的值和右侧小于pivot的值都找到了，因此可以交换位置
+            if (left <= right) {
+                int tmp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = tmp;
+
+                // ‘交换结束，要移动两个指针到下一个位置
+                left++;
+                right--;
+            }
+
+            // ‘重复上面的处理，直至循环结束
+        }
+
+        // ‘循环结束时，left 在 right右边，所以排序可以一分为二(start, right) (left, end)
+        quickSort(arr, start, right);
+        quickSort(arr, left, end);
+
+    }
+
+    public static void main(String[] args) {
+        int size = 10000000;
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = (int) (Math.random() * (size * 10));
+        }
+
+        long start = System.currentTimeMillis();
+        quickSort(arr, 0, arr.length - 1);
+        long end = System.currentTimeMillis();
+
+        System.out.printf("快速排序处理%d个元素的数组耗时%d秒", size, (end - start) / 1000);
+    }
+}
---
+++ src/main/java/lifei/sort/ShellSort.java
@@ -0,0 +1,85 @@
+package lifei.sort;
+
+/**
+ * “希尔排序也是一种插入排序，它是简单插入排序经过改进后的一个更高效的版本，也成为缩小增量排序
+ * 
+ * “基本思想： “希尔排序是把记录按下标的一定增量分组，对没组使用直接插入排序算法，随着增量逐渐减少， “每组包含的关键词越来越多，当增量减至1时，整个文件恰被分成一组，算法便终止
+ * 
+ * @author Fei.Li
+ *
+ */
+public class ShellSort {
+
+    /**
+     * “交换方式： 对有序序列在插入时采用交换法，并测试排序速度
+     * 
+     * @param arr
+     */
+    static void swapSort(int[] arr) {
+
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+
+        int count = 0;
+        int tmp = 0;
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+
+            for (int i = gap; i < arr.length; i++) {
+                for (int j = i - gap; j >= 0; j -= gap) {
+                    if (arr[j] > arr[j + gap]) {
+                        tmp = arr[j];
+                        arr[j] = arr[j + gap];
+                        arr[j + gap] = tmp;
+                    }
+                }
+            }
+            // System.out.printf("第%d轮排序后的数组是%s\n", (++count), Arrays.toString(arr));
+        }
+    }
+
+    /**
+     * “移动方式 ： 对有序序列在插入时采用移动法，并测试排序速度
+     * 
+     * @param arr
+     */
+    static void moveSort(int[] arr) {
+        // ’10，9，8，7，6，5，4，3，2，1
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+
+        // ‘增量gap，并逐步地缩小增量
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            // ‘从第gap个元素，逐个对其所在的组进行 直接插入排序
+            for (int i = gap; i < arr.length; i++) {
+                int j = i;
+                int tmp = arr[j];
+                if (arr[j] < arr[j - gap]) {
+                    while (j - gap >= 0 && tmp < arr[j - gap]) {
+                        // ‘移动
+                        arr[j] = arr[j - gap];
+                        j -= gap;
+                    }
+                    // ‘当推出while后，就给temp找到插入的位置
+                    arr[j] = tmp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int size = 100000;
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = (int) (Math.random() * (size * 10));
+        }
+
+        long before = System.currentTimeMillis();
+        swapSort(arr);
+        // moveSort(arr);
+        long after = System.currentTimeMillis();
+        System.out.printf("排序所花的时间是%d豪秒\n", after - before);
+
+    }
+}
